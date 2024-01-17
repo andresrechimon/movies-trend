fb6d3b409b53865e78798cf4a232c72492c70cc7
Rechimon, Andrés <andres.r1995@hotmail.com>
2024-01-17 10:57:14 -0300

    Initial Commit

---
+++ movies-trends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movies-trends
+//
+//  Created by Andrés Rechimon on 17/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
